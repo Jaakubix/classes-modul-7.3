6feef14e7ffe21706fc5e73c3dfe5801a820a5dd
Jakub Piskorz <kpiskorz13@gmail.com>
2021-11-23 10:58:19 +0100

    make function which create fake data

--- program.py
+++ program.py
@@ -1,3 +1,5 @@
+from faker import Faker
+fake_data = Faker()
 class BaseContact:
     def __init__(self,imie,nazwisko,mail,nrpryw):
         self.imie = imie
@@ -18,3 +20,17 @@ class BusinessContact(BaseContact):
     def label_length(self):
         self.length = len(imie) + len(nazwisko)
         print(self.length)
+def create_contacts():
+    x = int(input("Podaj rodzaj wizytowki 1-BaseContact  2-BusinessContact: "))
+    y = int(input("Podaj ilosc danych, ktore maja zostac wygenerowane"))
+    for _ in range(y):
+        if x == 1:
+            BaseContact.imie = fake_data.first_name()
+            BaseContact.nazwisko = fake_data.last_name()
+            BaseContact.mail = fake_data.safe_email()
+            BaseContact.nrpryw = fake_data.phone_number()
+        else:
+            BaseContact.imie = fake_data.first_name()
+            BaseContact.nazwisko = fake_data.last_name()
+            BaseContact.mail = fake_data.safe_email()
+            BusinessContact.nrsluz = fake_data.phone_number()
